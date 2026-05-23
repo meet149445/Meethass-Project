fa0d6f88beb5a8ee6a545b8c243fb3d3cf4af138
meet149445 <mp9569270@gmail.com>
2026-05-23 14:23:54 +0530

    Fixed authService import issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { loginUser } from "../services/authService";
+import { loginUser } from "../services/AuthService";
 
 import styles from "./Login.module.css";
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { registerUser } from "../services/authService";
+import { registerUser } from "../services/AuthService";
 
 import styles from "./Login.module.css";
 
